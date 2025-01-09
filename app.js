198497d17430ed93540cf6cb36053bc780fa453e
dee <178827694+dee797@users.noreply.github.com>
2025-01-09 14:31:20 -0700

    chore: Add middleware for catching errors

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const raceRouter = require("./routes/raceRouter");
-const charRouter = require("./routes/charRouter")
+const charRouter = require("./routes/charRouter");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use("/characters", charRouter)
+app.use("/characters", charRouter);
 app.use("/races", raceRouter);
 app.use("/", (req, res) => {
-    
+    res.render("index");
 });
 
+app.use((req, res, next) => {
+    res.status(404);
+    res.send("404 Error - Not Found");
+  });
 
+  app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(err.statusCode || 500).send(err.message);
+  });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
