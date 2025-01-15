c529b898b8aff51037b7e6d51aa48da486d90bdf
dee <178827694+dee797@users.noreply.github.com>
2025-01-14 18:00:13 -0700

    fix: remove unnecessary returns in functions that update db

--- db/charQueries.js
+++ db/charQueries.js
@@ -11,19 +11,19 @@ async function getChar(id) {
 }
 
 async function addChar({name, race, birth, death, gender, realm}) {
-  return await pool.query(`INSERT INTO character (name, race, birth, death, gender, realm) 
+  await pool.query(`INSERT INTO character (name, race, birth, death, gender, realm) 
                     VALUES ($1, $2, $3, $4, $5, $6)`, [name, race, birth, death, gender, realm]);
 }
 
 
 async function updateChar(id, {name, race, birth, death, gender, realm}) {
-  return await pool.query(`UPDATE character 
-                           SET name = $2, race = $3, birth = $4, death = $5, gender = $6, realm = $7
-                           WHERE character.id = $1`, [id, name, race, birth, death, gender, realm]);
+  await pool.query(`UPDATE character 
+                    SET name = $2, race = $3, birth = $4, death = $5, gender = $6, realm = $7
+                    WHERE character.id = $1`, [id, name, race, birth, death, gender, realm]);
 }
 
 async function deleteChar(id) {
-  return await pool.query(`DELETE FROM character WHERE character.id = $1`, [id]);
+  await pool.query(`DELETE FROM character WHERE character.id = $1`, [id]);
 }
 
 
