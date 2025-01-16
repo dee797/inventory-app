e7a37d0cec455fa72fdee644a792d73cd0c25bb6
dee <178827694+dee797@users.noreply.github.com>
2025-01-16 16:10:46 -0700

    chore: add functions for querying race table in db

--- db/raceQueries.js
+++ db/raceQueries.js
@@ -1,10 +1,37 @@
 const pool = require("./pool");
 
 async function getAllRaces() {
-  const { rows } = await pool.query("SELECT * FROM race");
+  const { rows } = await pool.query("SELECT * FROM race ORDER BY name");
   return rows;
+}
+
+async function getRace(name) {
+  const { rows } = await pool.query("SELECT * FROM race WHERE race.name = $1", [name]);
+    return rows[0];
+}
+
+async function getCharsOfRace(name) {
+  const { rows } = await pool.query("SELECT * FROM character WHERE character.race = $1", [name]);
+  return rows;
+}
+
+async function addRace(name) {
+  await pool.query("INSERT INTO race (name) VALUES ($1)", [name]);
+}
+
+async function updateRace(oldName, newName) {
+  await pool.query("UPDATE race SET name = $2 WHERE race.name = $1", [oldName, newName]);
+}
+
+async function deleteRace(name) {
+    await pool.query("DELETE FROM race WHERE race.name = $1", [name]);
 }
 
 module.exports = {
     getAllRaces,
+    getRace,
+    getCharsOfRace,
+    addRace,
+    updateRace,
+    deleteRace
 }
