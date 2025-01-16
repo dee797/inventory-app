2f52acd0e87cb498410752fbe545de7b3d7cf0d6
dee <178827694+dee797@users.noreply.github.com>
2025-01-16 14:07:35 -0700

    refactor: use CustomError for statuses other than 500
    The second-to-last app-level middleware will send a 500 status
    if no CustomError is thrown in any of the controllers

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
+const path = require("node:path");
 const express = require("express");
 const app = express();
-const raceRouter = require("./routes/raceRouter");
+//const raceRouter = require("./routes/raceRouter");
 const charRouter = require("./routes/charRouter");
 const racedb = require("./db/raceQueries");
 const realmdb = require("./db/realmQueries");
 const asyncHandler = require("express-async-handler");
 const CustomError = require("./errors/CustomError");
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
@@ -35,14 +37,14 @@ getRealms();
 
 
 app.use("/characters", charRouter);
-app.use("/races", raceRouter);
+//app.use("/races", raceRouter);
 app.get("/", (req, res) => {
     res.render("index");
 });
 
 app.use((err, req, res, next) => {
     console.error(err);
-    res.status(err.statusCode || 500).send(err.message);
+    res.status(err.statusCode || 500).send(err.message || "Internal server error");
 });
 
 app.all('/*', (req, res, next) => {
--- controllers/charController.js
+++ controllers/charController.js
@@ -5,20 +5,16 @@ const asyncHandler = require("express-async-handler");
 
 
 const getAllChars = asyncHandler(async (req, res) => {
-    try {
-      const chars = await db.getAllChars();
-      res.render("characters", {
-        title: "Character List",
-        chars: chars,
+    const chars = await db.getAllChars();
+    res.render("charViews/characters", {
+      title: "Character List",
+      chars: chars,
     });
-    } catch {
-      throw new CustomError(500, "Internal server error");
-    } 
 });
 
 
 const getAddChar = (req, res) => {
-  res.render("addChar", {
+  res.render("charViews/addChar", {
     title: "Add Character",
     races: req.app.locals.races || [{name: "Elf"}, {name: "Man"}, {name: "Hobbit"}],
     realms: req.app.locals.realms || [{name: "Rivendell"}, {name: "The Shire"}, {name: "Gondor"}]
@@ -30,20 +26,17 @@ const lengthErr = "must be between 1 and 50 characters.";
 
 const validateChar = [
   body("name").trim()
-    .isLength({ min: 1, max: 50 }).withMessage(`Name ${lengthErr}`)
-    .escape(),
+    .isLength({ min: 1, max: 50 }).withMessage(`Name ${lengthErr}`),
 
   body("birth")
     .optional({values: "falsy"})
     .trim()
-    .isLength({ min: 1, max: 50 }).withMessage(`Birthdate ${lengthErr}`)
-    .escape(),
+    .isLength({ min: 1, max: 50 }).withMessage(`Birthdate ${lengthErr}`),
 
   body("death")
     .optional({values: "falsy"})
     .trim()
     .isLength({ min: 1, max: 50 }).withMessage(`Date of death ${lengthErr}`)
-    .escape()
 ];
 
 
@@ -53,7 +46,7 @@ const postAddChar = [
     const errors = validationResult(req);
   
     if (!errors.isEmpty()) {
-      return res.status(400).render("addChar", {
+      return res.status(400).render("charViews/addChar", {
         title: "Add Character",
         errors: errors.array(),
       });
@@ -63,31 +56,27 @@ const postAddChar = [
   asyncHandler(async (req, res) => {
     const { name, race, birth, death, gender, realm } = req.body;
   
-    try {
-      await db.addChar({ name, race, birth, death, gender, realm });
-      res.redirect("/characters");
-    } catch {
-      throw new CustomError(500, "Internal server error");
-    }
+    await db.addChar({ name, race, birth, death, gender, realm });
+    res.redirect("/characters");
   })
 ];
 
 
 
-const getCharUpdate = asyncHandler(async (req, res) => {
-    
-  try {
+const getCharUpdate = asyncHandler(async (req, res) => {    
     const char = await db.getChar(parseInt(req.params.id));
+
+    if (!char) {
+      throw new CustomError(404, "Character not found");
+    }
+
     res.char = char;
-    res.render("updateChar", {
+    res.render("charViews/updateChar", {
       title: "Update Character",
       char: char,
       races: req.app.locals.races || [{name: "Elf"}, {name: "Man"}, {name: "Hobbit"}],
       realms: req.app.locals.realms || [{name: "Rivendell"}, {name: "The Shire"}, {name: "Gondor"}]
-    });
-  } catch {
-    throw new CustomError(404, "Character not found");
-  }    
+    });   
 });
   
 
@@ -97,7 +86,7 @@ const postCharUpdate = [
       const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).render("updateChar", {
+      return res.status(400).render("charViews/updateChar", {
         title: "Update Character",
         char: res.char,
         errors: errors.array(),
@@ -108,23 +97,15 @@ const postCharUpdate = [
   asyncHandler(async (req, res) => {
     const { name, race, birth, death, gender, realm } = req.body;
   
-    try {
-      await db.updateChar(parseInt(req.params.id), { name, race, birth, death, gender, realm });
+    await db.updateChar(parseInt(req.params.id), { name, race, birth, death, gender, realm });
       res.redirect("/characters");
-    } catch {
-      throw new CustomError(500, "Internal server error");
-    }
   })
 ];
 
 
 const postCharDelete = asyncHandler(async (req, res) => {
-    try {
-      await db.deleteChar(parseInt(req.params.id));
-      res.redirect("/characters");
-    } catch {
-      throw new CustomError(500, "Internal server error");
-    }
+    await db.deleteChar(parseInt(req.params.id));
+    res.redirect("/characters");
 });
 
 
