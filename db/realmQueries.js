24a04f0a3fd08bc3f34531d48f9ffd4efa47dbd6
dee <178827694+dee797@users.noreply.github.com>
2025-01-17 11:31:04 -0700

    chore: add more functions for querying realm table

--- db/realmQueries.js
+++ db/realmQueries.js
@@ -5,6 +5,34 @@ async function getAllRealms() {
   return rows;
 }
 
+async function getRealm(name) {
+  const { rows } = await pool.query("SELECT * FROM realm WHERE realm.name = $1", [name]);
+    return rows[0];
+}
+
+async function getCharsOfRealm(name) {
+  const { rows } = await pool.query("SELECT * FROM character WHERE character.realm = $1", [name]);
+  return rows;
+}
+
+async function addRealm(name) {
+  await pool.query("INSERT INTO realm (name) VALUES ($1)", [name]);
+}
+
+async function updateRealm(oldName, newName) {
+  await pool.query("UPDATE realm SET name = $2 WHERE realm.name = $1", [oldName, newName]);
+}
+
+async function deleteRealm(name) {
+    await pool.query("DELETE FROM realm WHERE realm.name = $1", [name]);
+}
+
+
 module.exports = {
     getAllRealms,
+    getRealm,
+    getCharsOfRealm,
+    addRealm,
+    updateRealm,
+    deleteRealm
 }
